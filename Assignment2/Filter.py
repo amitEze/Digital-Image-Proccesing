99111f1278f34befa6cfd58b9517ecb2b421f040
ezer6 <ezer69@gmail.com>
2022-12-06 14:20:08 +0200

    last

--- Assignment2/Filter.py
+++ Assignment2/Filter.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 from scipy import signal
@@ -14,9 +13,8 @@ imgPath = 'CuteTiger.jpg'
 
 
 # Read the original image
-img = cv2.imread(imgPath) 
+img = cv2.imread(imgPath,0)
 
-gray = cv2.imread(imgPath,flags=0)
 
 
 
@@ -25,60 +23,35 @@ gray = cv2.imread(imgPath,flags=0)
 cv2.imshow('Original', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+img = np.float64(img)
 
 #Choosing Mode
 mode=input("Choose mode between \n 1)edge2 detection \n 2)smoothing : ")
 if mode=='2':
     #Choosing kernel size:
-    kerSize=int(input("Enter the kernel size from 3,5,7,9"))    
+    kerSize=int(input("Enter the kernel size from 3,5,7,9 : "))    
     kernel = np.ones((kerSize,kerSize))
+    kernel = kernel/np.sum(kernel)
     output = signal.convolve2d(img,kernel)
-    output = output/np.sum(kernel)
-    cv2.imwrite('smoothed_img.jpg')
+    output = np.uint8(output)
+    cv2.imwrite('smoothed_img.jpg',output)
     cv2.imshow('smoothed',output)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
-    
+    #Edge detection
     kerX=np.array([[-1, 0 ,1],
                     [-2, 0, 2],
-                    [-1, 0, 1]])
+                    [-1, 0, 1]],dtype=np.float64)
 
     kerY=np.array([[1, 2, 1],
                     [0, 0, 0],
-                    [-1, -2, -1]])
-    # test_x_wise=signal.convolve2d(gray,kerX,"same")
-    # test_y_wise=signal.convolve2d(gray,kerY,"same")
-    # test_filter = np.sqrt(test_x_wise*test_x_wise+test_y_wise*test_y_wise)
-    # sovel = cv2.Sobel(gray,cv2.CV_16S,1,1,ksize=3,scale=1,delta=0,borderType=cv2.BORDER_DEFAULT)
-    # cv2.imwrite('sobeledPicture.jpg',sovel)
-    test_filter2d = filter2D(gray,kerX,kerY)
+                    [-1, -2, -1]],dtype=np.float64)
+
+
+    test_filter2d = filter2D(img,kerX,kerY)
+    test_filter2d = np.uint8(test_filter2d)
+    cv2.imshow('Sobel Edge Detection',test_filter2d)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     cv2.imwrite('test_filter2D.jpg',test_filter2d)
-    # cv2.imwrite('test_both_sides.jpg',test_filter)
-    # cv2.imwrite('test_x_wise.jpg',test_x_wise)
-    # cv2.imwrite('test_y_wise.jpg',test_y_wise)
-
-
-
-def rgb2Gray(img):
-    return np.dot(img[...,:3],[0.299,0.587,0.114])
-
-    
-
-
-kerI = np.array([[0,0,0],
-                 [0,1,0],
-                 [0,0,0]])
-
-
-
-
-
-
-
-
-# actual_filter=cv2.filter2D(img,ddepth=-1,kernel=kerX)
-# cv2.imshow('filtered',actual_filter)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
--- Assignment2/HelperFunctions.py
+++ Assignment2/HelperFunctions.py
@@ -1,27 +1,24 @@
 import numpy as np
-
+import cv2
 
 def filter2D(img, kernelX, kernelY):
-    kerSize = len(kernelX)
+    kerSize = len(kernelX)-1
     rows, cols = img.shape[:2]
     grad = np.zeros((rows, cols))
-
-    for y in range((kerSize-1)//2, rows-(kerSize-1)//2):
-        for x in range((kerSize-1)//2, cols-(kerSize-1)//2):
+    for y in range((kerSize)//2, rows-(kerSize)//2):
+        for x in range((kerSize)//2, cols-(kerSize)//2):
             Gx = 0
             Gy = 0
             for i in range(-kerSize//2, (kerSize//2)+1):
                 for j in range(-kerSize//2, (kerSize//2)+1):
                     val = img[y+i, x+j]
-                    Gx += int(kernelX[kerSize//2+i][kerSize//2+j]) * val
-                    Gy += int(kernelY[kerSize//2+i][kerSize//2+j]) * val
-            sqrt = int(np.sqrt(Gx*Gx + Gy*Gy))
-            if sqrt>255:
-                print(sqrt)
-            # if sqrt<140:
-            #     sqrt = 0
-            # else:
-            #     sqrt=255
-            grad[y,x] = sqrt
-
+                    Gx += kernelX[kerSize//2+i][kerSize//2+j] * val
+                    Gy += kernelY[kerSize//2+i][kerSize//2+j] * val
+            
+            sqrt = round(np.sqrt(Gx*Gx + Gy*Gy))
+            grad[round(y-kerSize/2),round(x-kerSize/2)] = sqrt
     return grad
+
+def smooth_fliter(img,ker):
+    kerSize = len(ker)
+    rows, cols = img.shape[:2]
